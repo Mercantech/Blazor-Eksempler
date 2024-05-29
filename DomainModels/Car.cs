9a02111e178f342d2188c6f917b172fa6c393d18
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2024-05-29 13:59:35 +0200

    Images

---
+++ DomainModels/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace DomainModels
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public List<string> Images { get; set; }
+    }
+}
